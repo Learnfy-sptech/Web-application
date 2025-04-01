3553cfaccb67b9a1b9f3a3e52d7e6fafb46e78af
ViniciusAlvesF3Capital <vsantos@f3capital.com.br>
2025-04-01 14:22:29 -0300

    Protótipo dash

--- Site/public/assets/javascript/script.js
+++ Site/public/assets/javascript/script.js
@@ -165,3 +165,45 @@ document.addEventListener('DOMContentLoaded', function() {
 
     toggleIconVisibility();
 });
+
+const ctx1 = document.getElementById('ChartOne');
+
+  new Chart(ctx1, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  const ctx2 = document.getElementById('ChartTwo');
+
+  new Chart(ctx2, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
