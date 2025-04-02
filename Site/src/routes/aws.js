4226badee9f12a1333221f78a3936841195890b3
Guilherme-Gotardo <guilherme.santos043@sptech.school>
2025-04-02 18:31:04 -0300

    Correção das variaveis de ambiente da AWS

--- Site/src/routes/aws.js
+++ Site/src/routes/aws.js
@@ -10,8 +10,8 @@ const upload = multer({ storage: storage });
 dotenv.config();
 const bucketName = process.env.BUCKET_NAME
 const bucketRegion = process.env.BUCKET_REGION
-const accessKey = process.env.ACCESS_KEY
-const secretAccess_key = process.env.SECRET_ACCESS_KEY
+const accessKey = process.env.AWS_ACCESS_KEY_ID
+const secretAccess_key = process.env.AWS_SECRET_ACCESS_KEY
 
 const s3 = new S3Client({
     region: bucketRegion,
