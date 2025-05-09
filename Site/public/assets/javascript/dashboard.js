b12676283221fcb2fdf2f9494239a479f2a09ffe
ViniciusAlvesF3Capital <vsantos@f3capital.com.br>
2025-05-09 11:53:29 -0300

    att kpis dashes e melhora na ux

--- Site/public/assets/javascript/dashboard.js
+++ Site/public/assets/javascript/dashboard.js
@@ -2,10 +2,6 @@ const nome = sessionStorage.getItem("NOME_USUARIO");
 
 const userName = document.getElementById('name-user');
 
-window.onload = function (nome) {
-  userName.innerHTML = nome ?? "Usuário";
-}
-
 function clear() {
   sessionStorage.removeItem(EMAIL_USUARIO );
   sessionStorage.removeItem(NOME_USUARIO);
