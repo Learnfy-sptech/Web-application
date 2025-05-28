f87a4d367d19f49e0e98baedee543850463501dc
lucasaiello-dev <lucas.aiello@sptech.school>
2025-05-28 18:10:07 -0300

    Ajustando o campo de input do nome do relatório

--- Site/public/assets/javascript/relatorio.js
+++ Site/public/assets/javascript/relatorio.js
@@ -11,3 +11,38 @@ function trocarTelaRelatorio() {
     infoRelatorio.classList.toggle("oculto");
 
 }
+
+var inputPreenchido = false
+function limparCampo(idElemento) {
+    elemento = document.getElementById(idElemento)
+    if (inputPreenchido == false) {
+        inputPreenchido = true
+        elemento.value = ""
+    }
+    elemento.focus()
+}
+
+function validarInputVazio(idElemento, texto) {
+    elemento = document.getElementById(idElemento)
+    if (elemento.value.length == 0) {
+        elemento.value = texto
+        inputPreenchido = true
+    }
+}
+
+function abrirOuFecharTodasColunas() {
+    elemento = document.getElementById('todas_colunas')
+    elemento.classList.toggle('oculto')
+    elementoPai = document.getElementsByTagName("main")[0]
+    elemento.classList.toggle('desfoque')
+}
+
+function adicionarColuna(elemento) {
+
+}
+
+function removerColuna(elemento) {
+
+}
+
+var dadosColunasRelatorio = []
